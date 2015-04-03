647265e1dff6bcb1757aa78e5294a9cdc36fca34
Prakash Manandhar <pmanandhar@debian-pm>
2015-04-02 23:30:03 -0400

    Trying unicode...

--- src/ImageServer/ImageServer.cpp
+++ src/ImageServer/ImageServer.cpp
@@ -1,16 +1,23 @@
 
 #include "ImageServer.h"
+#include <iostream>
 #include <stdexcept>
+#include <string>
 
 // example of a namespace alias
 namespace FS = LMFocusStack;
 
 FS::ImageServer::ImageServer(int argc, char ** argv) {
-	
+
+	const std::string help = "--help";
 	if (argc < 2) { // settings file not specified, use default
 		
 	} else { // load settings or display help
-		
+		if (argv[1] == help) {
+			//std::string test =L"Latin text на кирилица and more latin";
+			std::cout << "हामिछ" << std::endl;
+			//std::cout << "Help Message" << std::endl;
+		}
 	} 
 }
 

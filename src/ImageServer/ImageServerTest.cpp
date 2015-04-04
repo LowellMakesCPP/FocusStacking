099e7dd988b2d761cada73a61656e1f8d7b0a518
tbonza <tylers.pile@gmail.com>
2015-04-04 18:21:22 -0400

    exit after help message

--- src/ImageServer/ImageServerTest.cpp
+++ src/ImageServer/ImageServerTest.cpp
@@ -1,16 +1,24 @@
-
+#include <iostream>
 #include "ImageServer.h"
 
 // PManandhar note: not preferred, used for demo
 // or short files
 using namespace LMFocusStack;
+using namespace std;
 
 int main(int argc, char ** argv) {
 
+  try {
 	ImageServer server(argc, argv);
 
-
 	return 0;
-
+	
+  }
+  catch (exception &e) {
+    // Tyler note: http://isocpp.org/wiki/faq/exceptions#ctors-can-throw
+        std::cerr << "Program closing: " << e.what();
+        return -1;
+  }
+ 
 }
 
